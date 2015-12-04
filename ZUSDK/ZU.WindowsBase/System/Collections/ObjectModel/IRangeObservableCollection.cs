06b0adc894d6a059410c6de4013aede9171170f5
daniel@zetuniverse.com <daniel@zetuniverse.com>
2015-12-05 00:56:35 +0300

    removed unnecessary contents (WACP, ZU.PresentationFramework, ZU.WindowsBase)

--- ZUSDK/ZU.WindowsBase/System/Collections/ObjectModel/IRangeObservableCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿//using System;
-//namespace ZU.Collections.ObjectModel
-//{
-//	public interface IRangeObservableCollection<T>
-//	{
-//		void AddRange(global::System.Collections.Generic.IEnumerable<T> list);
-//		void NotifyRefreshCollection();
-//	} // interface
-//} // namespace
