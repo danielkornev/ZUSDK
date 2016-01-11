89b9a560679a2492a50fccbe4ce7bde6bd7a7e41
daniel@zetuniverse.com <daniel@zetuniverse.com>
2016-01-11 14:55:48 +0300

    added full-text attribute

---
+++ ZUSDK/ZU.Core.Developer/Semantic/Text/FullTextAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZU.Semantic.Text
+{
+	[AttributeUsage(AttributeTargets.Class |
+	AttributeTargets.Method,
+	AllowMultiple = false)]
+	public class FullTextAttribute : System.Attribute
+	{
+		private string kindAliases;
+		private string kindDescription;
+
+		public FullTextAttribute(string lcKindAliases, string lcKindDescription)
+		{
+			this.kindAliases = lcKindAliases;
+			this.kindDescription = lcKindDescription;
+		}
+
+		public string KindAliases
+		{
+			get
+			{
+				return this.kindAliases;
+			}
+			set
+			{
+				this.kindAliases = value;
+			}
+		}
+
+		public string KindDescription
+		{
+			get
+			{
+				return this.kindDescription;
+			}
+			set
+			{
+				this.kindDescription = value;
+			}
+		}
+	} // class
+} // namespace
